a317e135fe40bca7de58f16642348685d2d80588
emiliano2026 <ealvarezzanelli@gmail.com>
2026-09-21 09:50:50 -0300

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,19 @@ except:
     st.sidebar.warning("Logo no encontrado. Subí 'LogoIIPAC.jpg'.")
 
 st.title("Dashboard de Sequía 2020-2023")
-st.caption("Análisis de la intensidad y duración de la sequía en relación a las resoluciones de emergencia declaradas. El valor de sequía representado corresponde al valor acumulado trimestral, sumatoria de los valores de intensidad de sequía leve=1, moderada=2 y severa=3.")
+#st.caption("Análisis de la intensidad y duración de la sequía en relación a las resoluciones de emergencia declaradas. El valor de sequía representado corresponde al valor acumulado trimestral, sumatoria de los valores de intensidad de sequía leve=1, moderada=2 y severa=3. El valor representado en el gráfico sintetiza la intensidad y duración de la sequía")
+st.markdown(
+    """
+    <div style="font-size: 17px; line-height: 1.6; color: #444; margin-bottom: 1.5rem;">
+        <b>Análisis de la intensidad y duración de la sequía</b> en relación a las resoluciones de emergencia declaradas.<br>
+        El valor de sequía representado corresponde al valor acumulado trimestral, sumatoria de los valores de intensidad de sequía:
+        leve = 1, moderada = 2 y severa = 3 El valor representado en el gráfico sintetiza la intensidad y duración de la sequía.
+    </div>
+    """,
+    unsafe_allow_html=True,
+)
+
+
 # ─────────────────────────────────────────────────────────────────────
 # FUNCIONES AUXILIARES
 # ─────────────────────────────────────────────────────────────────────
